76ad3ca703dfa8c2dd3d76e18eb2ce287b65d37a
Naozumi <andy@amackillop.com>
2017-09-15 16:33:34 +0100

    Added serialization functionality, so that can work to deserialize later, and understand the JSON output that is expected.

--- cs-updater/MainWindow.xaml.cs
+++ cs-updater/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,7 +31,46 @@ namespace cs_updater
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            
+
+            DirectoryInfo dir = new DirectoryInfo("C:\\cstest\\");
+
+            var jsonObject = BuildStructure(dir);
+
+            string output = JsonConvert.SerializeObject(jsonObject, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            rtf_news.Document.Blocks.Clear();
+            rtf_news.Document.Blocks.Add(new Paragraph(new Run(output)));
+
 
         }
+
+        private static List<Node> BuildStructure(DirectoryInfo directory)
+        {
+            var jsonObject = new List<Node>();
+
+            foreach (var file in directory.GetFiles())
+            {
+                var crc = string.Empty;
+                using (FileStream stream = File.OpenRead(file.FullName))
+                {
+                    using (SHA1Managed sha = new SHA1Managed())
+                    {
+                        byte[] checksum = sha.ComputeHash(stream);
+                        crc = BitConverter.ToString(checksum)
+                            .Replace("-", string.Empty).ToLower();
+                    }
+                }
+                jsonObject.Add(new Node("file", file.Name, crc));
+            }
+
+            foreach (var folder in directory.GetDirectories())
+            {
+                jsonObject.Add(new Node("folder", folder.Name, BuildStructure(new DirectoryInfo(folder.FullName))));
+            }
+
+            return jsonObject;
+        }
+
     }
 }
---
+++ cs-updater/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_updater
+{
+    class Node
+    {
+        public string type { get; set; }
+        public string name { get; set; }
+        public string crc { get; set; }
+        public List<Node> children { get; set; }
+
+        public Node(string type, string name, string crc)
+        {
+            this.type = type;
+            this.name = name;
+            this.crc = crc;
+        }
+
+        public Node(string type, string name, List<Node> children)
+        {
+            this.type = type;
+            this.name = name;
+            this.children = children;
+        }
+    }
+}
