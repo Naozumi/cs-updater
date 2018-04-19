3b56fbd2ba9697d0320346f303f48321bf2ec7b0
Naozumi <andy@amackillop.com>
2018-04-19 16:34:15 +0100

    Add button choices and return values to the Notification Window.

--- cs-updater/NotificationWindow.xaml.cs
+++ cs-updater/NotificationWindow.xaml.cs
@@ -19,9 +19,10 @@ namespace cs_updater
     /// </summary>
     public partial class NotificationWindow : Window
     {
-        public NotificationWindow(string title, List<NotificationWindowItem> items)
+        public NotificationWindow(string title, List<NotificationWindowItem> items, int options = 0)
         {
             InitializeComponent();
+            Result = 0;
             LocUtil.SetDefaultLanguage(this);
 
             string ti = this.FindResource(title) as string;
@@ -38,15 +39,44 @@ namespace cs_updater
                 {
                     textblock.Text = item.text;
                 }
-                
+
                 textblock.TextWrapping = TextWrapping.Wrap;
                 TextArea.Children.Add(textblock);
             }
+
+            switch (options){
+                case 0:
+                    Btn_1.SetResourceReference(System.Windows.Controls.Button.ContentProperty, "Close");
+                    Btn_1.Visibility = Visibility.Visible;
+                    Btn_2.Visibility = Visibility.Collapsed;
+                    break;
+                case 1:
+                    Btn_1.SetResourceReference(System.Windows.Controls.Button.ContentProperty, "OK");
+                    Btn_1.Visibility = Visibility.Visible;
+                    Btn_2.SetResourceReference(System.Windows.Controls.Button.ContentProperty, "Cancel");
+                    Btn_2.Visibility = Visibility.Visible;
+                    break;
+                case 2:
+                    Btn_1.SetResourceReference(System.Windows.Controls.Button.ContentProperty, "Yes");
+                    Btn_1.Visibility = Visibility.Visible;
+                    Btn_2.SetResourceReference(System.Windows.Controls.Button.ContentProperty, "No");
+                    Btn_2.Visibility = Visibility.Visible;
+                    break;
+            }
         }
 
-        private void Close_Click(object sender, RoutedEventArgs e)
+        private void Btn1_Click(object sender, RoutedEventArgs e)
         {
+            this.Result = 1;
             this.Close();
         }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Result = -1;
+            this.Close();
+        }
+
+        public int Result { get; private set; }
     }
 }
